1df40a8c2c87ac631202e42ceb581e074ad83aa5
Adri Ana <szopiangreg@gmail.com>
2021-02-27 21:38:17 +0000

    TimelineMax() is working

---
+++ script.js
@@ -0,0 +1,8 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector("slider");
+const logo = document.querySelector("#logo");
+const hamburger = document.querySelector(".hamburger");
+const headline = document.querySelector(".headline");
+
+var tl = new TimelineMax();
+tl.fromTo(hero, 1, { height: "0%" }, { height: "80%" });
